5d0141815db23ed5d5af4ebaf06a67094f5f4740
ML-czy <1601430035@qq.com>
2021-12-19 19:18:24 +0800

    助手

---
+++ 代码/倒序输出数.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	char a[10];
+	for(i=0;i<10;i++)
+	{
+		scanf("%c",&a[i]);
+	 }
+	 for(i=9;i>=0;i--)
+	 {
+	 	printf("%d",i,a[i]);
+	 }
+	 	
+	 	
+	 
+	
+	return 0;
+}
